70069d3e2f9234e45d7a8b2d6bf986e6752a1b52
Rover Chen <rover.k.chen@gmail.com>
2024-09-18 16:52:11 +0800

    scouting

---
+++ scouting.php
@@ -0,0 +1,71 @@
+<?php
+/**
+ * Plugin Name: scouting
+ * Plugin URI: https://wordpress.org/plugins/scouting/
+ * Description: The leading documents management plugin for scouting system by shortcode
+ * Author: dgc.network
+ * Author URI: https://dgc.network/
+ * Version: 0.0.1
+ * Requires at least: 6.0
+ * Tested up to: 6.5.3
+ *
+ * Text Domain: scouting
+ * Domain Path: /languages/
+ */
+
+if (!defined('ABSPATH')) {
+    exit;
+}
+/*
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+*/
+function register_session() {
+    if ( ! session_id() ) {
+        session_start();
+    }
+}
+add_action( 'init', 'register_session' );
+
+function remove_admin_bar() {
+    if (!current_user_can('administrator') && !is_admin()) {
+      show_admin_bar(false);
+    }
+}
+add_action('after_setup_theme', 'remove_admin_bar');  
+
+function allow_subscribers_to_view_users($allcaps, $caps, $args) {
+    // Check if the user is trying to view other users
+    if (isset($args[0]) && $args[0] === 'list_users') {
+        // Check if the user has the "subscriber" role
+        $user = wp_get_current_user();
+        if (in_array('subscriber', $user->roles)) {
+            // Allow subscribers to view users
+            $allcaps['list_users'] = true;
+        }
+    }
+    return $allcaps;
+}
+add_filter('user_has_cap', 'allow_subscribers_to_view_users', 10, 3);
+
+function get_post_type_meta_keys($post_type) {
+    global $wpdb;
+    $query = $wpdb->prepare("
+        SELECT DISTINCT(meta_key)
+        FROM $wpdb->postmeta
+        INNER JOIN $wpdb->posts ON $wpdb->posts.ID = $wpdb->postmeta.post_id
+        WHERE $wpdb->posts.post_type = %s
+    ", $post_type);
+    return $wpdb->get_col($query);
+}
+
+function isURL($str) {
+    $pattern = '/^(http|https):\/\/[^ "]+$/';
+    return preg_match($pattern, $str) === 1;
+}
+
+//require_once plugin_dir_path( __FILE__ ) . 'erp/erp-cards.php';
+//require_once plugin_dir_path( __FILE__ ) . 'erp/subforms.php';
+require_once plugin_dir_path( __FILE__ ) . 'services/services.php';
+//require_once plugin_dir_path( __FILE__ ) . 'includes/iso-helper.php';

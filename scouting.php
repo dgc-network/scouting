d06c2b3c89c14ae47dcda35280b158d105bb16a5
Rover Chen <rover.k.chen@gmail.com>
2024-09-19 14:45:19 +0800

    //add_action('after_setup_theme', 'remove_admin_bar');

--- scouting.php
+++ scouting.php
@@ -21,11 +21,11 @@ if (!session_id()) {
     session_start();
 }
 
-/*
+
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
-*/
+
 function register_session() {
     if ( ! session_id() ) {
         session_start();

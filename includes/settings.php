fe98c9a9b9e32e4cf924f22ad736bec9d17de4f6
Rover Chen <rover.k.chen@gmail.com>
2024-09-22 06:23:57 +0800

    define('OA_SOCIAL_LOGIN_PLUGIN_URL', plugins_url() . '/' . basename(dirname(__FILE__))); define('OA_SOCIAL_LOGIN_BASE_PATH', dirname(plugin_basename(__FILE__))); define('OA_SOCIAL_LOGIN_VERSION', '5.9.0'); define('OA_SOCIAL_LOGIN_DEFAULT_THEME', 1);
    /**
     * Check technical requirements before activating the plugin (Wordpress 3.0 or newer required)
     */
    function oa_social_login_activate()
    {
        if (!function_exists('register_post_status'))
        {
            deactivate_plugins(basename(dirname(__FILE__)) . '/' . basename(__FILE__));
            echo sprintf(__('This plugin requires WordPress %s or newer. Please update your WordPress installation to activate this plugin.', 'oa-social-login'), '3.0');
            exit;
        }
        update_option('oa_social_login_activation_message', 0);
    }
    
    register_activation_hook(__FILE__, 'oa_social_login_activate');
    
    /**
     * Add Setup Link
     **/
    function oa_social_login_add_setup_link($links, $file)
    {
        static $oa_social_login_plugin = null;
    
        if (is_null($oa_social_login_plugin))
        {
            $oa_social_login_plugin = plugin_basename(__FILE__);
        }
    
        if ($file == $oa_social_login_plugin)
        {
            $settings_link = '<a href="admin.php?page=oa_social_login_setup">' . __('Setup', 'oa-social-login') . '</a>';
            array_unshift($links, $settings_link);
        }
    
        return $links;
    }
    
    add_filter('plugin_action_links', 'oa_social_login_add_setup_link', 10, 2);
    
    /**
     * Log
     */
    function oa_social_login_log($message)
    {
        if (defined('WP_DEBUG_LOG') && WP_DEBUG_LOG)
        {
            error_log('[OneAll Social Login] ' . date("d.m.Y G:i") . ' : ' . print_r($message, true));
        }
    }
    
    /**
     * This file only has to be included for versions before 3.1.
     * Deprecated since version 3.1, the functions are included by default
     */
    if (!function_exists('email_exists'))
    {
        require_once ABSPATH . WPINC . '/registration.php';
    }
    
    /**
     * Include required files
     */
    
    require_once dirname(__FILE__) . '/includes/settings.php';
    require_once dirname(__FILE__) . '/includes/communication.php';
    require_once dirname(__FILE__) . '/includes/toolbox.php';
    require_once dirname(__FILE__) . '/includes/admin.php';
    require_once dirname(__FILE__) . '/includes/user_interface.php';
    require_once dirname(__FILE__) . '/includes/widget.php';

---
+++ includes/settings.php
@@ -0,0 +1,149 @@
+<?php
+
+/**
+ * Available Providers
+ */
+$oa_social_login_providers = array(
+    'amazon' => array(
+        'name' => 'Amazon'
+    ),
+    'apple' => array(
+        'name' => 'Apple'
+    ),
+    'battlenet' => array(
+        'name' => 'Battle.net'
+    ),
+    'blogger' => array(
+        'name' => 'Blogger'
+    ),
+    'discord' => array(
+        'name' => 'Discord'
+    ),
+    'disqus' => array(
+        'name' => 'Disqus'
+    ),
+    'dribbble' => array(
+        'name' => 'Dribbble'
+    ),
+    'draugiem' => array(
+        'name' => 'Draugiem'
+    ),
+    'epicgames' => array(
+        'name' => 'Epic Games'
+    ),
+    'facebook' => array(
+        'name' => 'Facebook',
+        'is_default' => 1
+    ),
+    'foursquare' => array(
+        'name' => 'Foursquare'
+    ),
+    'github' => array(
+        'name' => 'Github.com'
+    ),
+    'google' => array(
+        'name' => 'Google',
+        'is_default' => 1
+    ),
+    'instagram' => array(
+        'name' => 'Instagram'
+    ),
+    'line' => array(
+        'name' => 'Line'
+    ),
+    'linkedin' => array(
+        'name' => 'LinkedIn',
+        'is_default' => 1
+    ),
+    'livejournal' => array(
+        'name' => 'LiveJournal'
+    ),
+    'mailru' => array(
+        'name' => 'Mail.ru'
+    ),
+    'meetup' => array(
+        'name' => 'Meetup'
+    ),
+    'mixer' => array(
+        'name' => 'Mixer'
+    ),
+    'odnoklassniki' => array(
+        'name' => 'Odnoklassniki'
+    ),
+    'openid' => array(
+        'name' => 'OpenID'
+    ),
+    'patreon' => array(
+        'name' => 'Patreon'
+    ),
+    'paypal' => array(
+        'name' => 'PayPal'
+    ),
+    'pinterest' => array(
+        'name' => 'Pinterest'
+    ),
+    'pixelpin' => array(
+        'name' => 'PixelPin'
+    ),
+    'reddit' => array(
+        'name' => 'Reddit'
+    ),
+    'skyrock' => array(
+        'name' => 'Skyrock.com'
+    ),
+    'soundcloud' => array(
+        'name' => 'SoundCloud'
+    ),
+    'spotify' => array(
+        'name' => 'Spotify'
+    ),
+    'stackexchange' => array(
+        'name' => 'StackExchange'
+    ),
+    'steam' => array(
+        'name' => 'Steam'
+    ),
+    'strava' => array(
+        'name' => 'Strava'
+    ),
+    'tumblr' => array(
+        'name' => 'Tumblr'
+    ),
+    'tiktok' => array(
+        'name' => 'TikTok'
+    ),
+    'twitch' => array(
+        'name' => 'Twitch.tv'
+    ),
+    'twitter' => array(
+        'name' => 'X',
+        'is_default' => 1
+    ),
+    'vimeo' => array(
+        'name' => 'Vimeo'
+    ),
+    'vkontakte' => array(
+        'name' => 'VKontakte'
+    ),
+    'weibo' => array(
+        'name' => 'Weibo'
+    ),
+    'windowslive' => array(
+        'name' => 'Windows Live'
+    ),
+    'wordpress' => array(
+        'name' => 'WordPress.com'
+    ),
+    'xing' => array(
+        'name' => 'Xing'
+    ),
+    'yahoo' => array(
+        'name' => 'Yahoo'
+    ),
+    'yandex' => array(
+        'name' => 'Yandex'
+    ),
+    'youtube' => array(
+        'name' => 'YouTube'
+    )
+);
